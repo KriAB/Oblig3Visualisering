77605f8cff47957f78d63dfd139cc91162d9a072
Benedicte Slettum <benesl@live.no>
2019-10-30 16:40:38 +0100

    B-Splines
    NPC
    -Controlpoints
    -EndPoints
    -Knotes

--- FSM/npc.cpp
+++ FSM/npc.cpp
@@ -6,21 +6,104 @@ NPC::NPC()
     mBSplineCurve = new BSplineCurve();
 }
 
+NPC::NPC(gsl::Vector3D *EndP, std::vector<gsl::Vector3D> itemP)
+{
+    mBSplineCurve = new BSplineCurve();
+
+    endPoints[0] = EndP[0];
+    endPoints[1] = EndP[1];
+
+    items = itemP;
+}
+
 NPC::~NPC()
 {
     delete  mBSplineCurve;
     mBSplineCurve = nullptr;
 }
 
+//----------------------CONTROLLPOINT-LOGIC___________________________________
+
+//Sett kontrollpunkter med items og endepunkter 1.time
+//Fylle inn kontrollpunker "controllPoints"
+//for(int i = 0; i< itemSize(); i++)
+//itemIndex for item {0,1,2,3...}
+//når switch skjer, vet rekkefølgen, endre rekkefølge
+
+//When a spline curve has n control points we need n+d +1 knots
+//since the curve is constructed by blending control points with B-spline basis functions:
+
+//n control points c0, c1,... cn−1
+
+//Sette kontrollpunktene som skal sendes til b-Spline
+
+//controllPoints.push_back(endPoints[0]);
+//for(int i = 0; i <itemsIndex.size(); i++)
+//{
+//    controllPoints.push_back(items.at(itemsIndex.at(i)));
+//}
+//controllPoints.push_back(endPoints[1]);
+
+//------------------IGNORE---------------------------------------------------------------
+
+void NPC::setControlPoint()
+{
+    controllPoints.push_back(endPoints[0]);
+    for(int i = 0; i < items.size(); i++)
+    {
+        controllPoints.push_back(items.at(i));
+    }
+
+    controllPoints.push_back(endPoints[1]);
+    for(int i= 0; i < items.size(); i++)
+    {
+        itemsIndex.push_back(i);
+    }
+}
+
+void NPC::makeKnots()
+{
+    //Lag skjøter basert på antall controllpunker(endepunkter)
+}
+
+void NPC::patrol()
+{
+    //Vite om Kontrollpunkt, Endepunkt
+    //if nådd endepunkt
+    // notify(0)
+    //StateChange
+    //State: Patrol - Learn
+    //If Item tatt
+    //notify(1);
+    //If player detected
+    //notify(2);
+    //If hinder i path
+    //notify(3)
+
+}
+
+void NPC::learn()
+{
+    //
+
+}
+
+void NPC::notify(int notification)
+{
+    notification_queue.push(notification);
+
+}
+
+
 void NPC::build_path()
 {
 
     // Bytte rekkefølge på ItemsIndex.
     if(iterator < itemsIndex.size()-1)
-  {
+    {
         std::iter_swap(itemsIndex.begin()+iterator, itemsIndex.begin()+iterator+1);
         iterator++;
-  }
+    }
     //Når man har gått igjennom alle indeksene så resette den.
     else
     {
@@ -28,6 +111,7 @@ void NPC::build_path()
         std::sort(itemsIndex.begin(), itemsIndex.end());
 
     }
+
     controllPoints.clear();
     controllPoints.resize(0);
 
--- FSM/npc.h
+++ FSM/npc.h
@@ -10,6 +10,9 @@ enum NPCStates
     SLEEP,PATROL, LEARN, CHASE
 };
 enum NPSEvents
+
+        //      0               1               2               3
+
 {
     ENDPOINT_ARRIVED, ITEM_TAKEN, PLAYER_DETECTED, OBSTACLE_DETECTED
 };
@@ -18,7 +21,17 @@ class NPC
 {
 public:
     NPC();
+    NPC(gsl::Vector3D *EndP, std::vector<gsl::Vector3D> itemP);
     ~NPC();
+
+    //Sett kontrollpunkter med items og endepunkter 1.time
+    //Fylle inn kontrollpunker "controllPoints"
+    void setControlPoint();
+
+    void makeKnots();
+
+
+
     //FSM part (Final State Machine)
     //
     void patrol(); //når den har gått en hel runde, kan den lære at den skal gå en annen rute neste gang.
@@ -37,7 +50,11 @@ private:
 
     //FSM part
     int state{0};
+    //NPC får sender en notifikasjon til seg selv hvis et object er borte
+    //NPC sender notifikasjon om spiller oppdaget.
+    //NPC sender notifikasjon om endepunkt er nådd.
     void notify(int notification); //Må gå til endepunkt før den endrer på kurven, slik som når den skal chase.
+    //Notifikasjonene blir lagt i en kø, når NPC'en ikke kan sende en notifikasjon øyeblikkelig.
     std::queue<int> notification_queue;
     float elapsed_time;
     //B-spline kurven har en skøtvektor, f.eks. {0,0,0,1,2,3,3,3}
@@ -49,13 +66,22 @@ private:
     //gjøre en permutasjon av kontrollpunktene(kun items, ikke endepunkter) Dette vil si at man bytter om på rekkefølgen til kontrollpunktene
     //vikting å ikke endre endepunktene
 
+
+    //Sender inn array med items sin posisjon, og en array med endepunktene
+    //item sine posisjoner = kontrollpunkter(endepunkter)
+
+    //Endepunkter
     gsl::Vector3D endPoints[2];
+    //Vector med items to collect
     std::vector<gsl::Vector3D> items;
     std::vector<int> itemsIndex;
+    //Kontrollpunkter
     std::vector<gsl::Vector3D> controllPoints;
     //for build_path();
     int iterator{0};
 
 };
+
+//
 
 #endif // NPC_H
